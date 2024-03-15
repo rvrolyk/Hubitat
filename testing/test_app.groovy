9c43a0acfd2e75942e4ab14fc43defa2b81f133d
rvrolyk <47088852+rvrolyk@users.noreply.github.com>
2024-03-15 13:46:44 -0700

    test bundles

---
+++ testing/test_app.groovy
@@ -0,0 +1,12 @@
+#include rvrolyk.SleepNumberLib
+
+definition(
+  name: APP_NAME,
+  namespace: NAMESPACE,
+  author: 'Russ Vrolyk',
+  description: 'Control your Sleep Number Flexfit bed.',
+  category: 'Integrations',
+  iconUrl: '',
+  iconX2Url: '',
+)
+
